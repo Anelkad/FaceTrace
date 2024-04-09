4026acf4a8fbb178ff6732e4221c39e31e10af86
Anelkad <anel.k@1fit.app>
2024-04-09 13:34:49 +0500

    bad request

--- data/src/main/java/com/example/data/remote/FaceTraceApi.kt
+++ data/src/main/java/com/example/data/remote/FaceTraceApi.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Part
 
 interface FaceTraceApi {
     @Multipart
-    @POST("api/v1/search")
+    @POST("api/v1/search/")
     suspend fun search(
         @Part file: MultipartBody.Part
     ): Response<List<SearchResultDTO>>
